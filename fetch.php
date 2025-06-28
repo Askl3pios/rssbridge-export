3177f78444d944220cd41fd722280f4ef30e2a68
Askl3pios <github.front744@slmails.com>
2025-06-28 11:32:06 +0200

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,6 @@
 <?php
 // Hämta flödet från din publika RSSBridge
-$source = 'https://rss-bridge.org/bridge01/?action=display&bridge=GoComicsBridge&comicname=brewsterrockit&date-in-title=on&limit=10&format=Atom';
+$source = 'https://rss-bridge.org/bridge01/?action=display&bridge=GoComicsBridge&comicname=garfield&date-in-title=on&limit=10&format=Atom';
 $target = __DIR__ . '/brewsterrockit.xml';
 
 $data = file_get_contents($source);
